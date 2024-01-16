349f1c8aa56a8fd6d1851518a3fc09eac60763bc
Serge Junior Szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-01-16 10:07:32 +0100

    Added second part of exo2

--- Exo2/Exo2B.java
+++ Exo2/Exo2B.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Exo2B {
+public class Exo2B {
+    static void sqrRoot (int num) {
+        int squareRoot = (int) Math.sqrt(num);
+        int result = squareRoot * squareRoot;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        sqrRoot(63);
+        sqrRoot(30);
+    }
 }
