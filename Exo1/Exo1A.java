038cad2d51ebae31898812f04e337ec1893e44c2
Serge Junior Szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-01-16 09:20:20 +0100

    Added exo Part 1 of exercice 1

---
+++ Exo1/Exo1A.java
@@ -0,0 +1,10 @@
+public class Exo1A {
+    public static void main(String[] args) {
+
+        int i = 1; // Always declare the data type
+        while (i < 101) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
