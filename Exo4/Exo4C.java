d3758a3340401bb4939c3a1614a627c8d4c83b18
Serge Junior Szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-01-16 10:47:11 +0100

    Added part 3 to exo4

---
+++ Exo4/Exo4C.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class Exo4C {
+    public static void describeNamesLength(ArrayList<String> names) {
+        for (String name : names) {
+            if (name.length() >= 4) {
+                System.out.println(name);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Jean");
+        names.add("Ahmed");
+        names.add("Lea");
+        names.add("Blanca");
+        describeNamesLength(names); // Corrected method name
+    }
+}
