4c709a1d49a86268a4d890ff30e2e6a5a57d9e2e
Serge Junior Szynkarski <serge.szynkarski@le-campus-numerique.fr>
2024-01-16 09:41:46 +0100

    Added the first part of Exo3

---
+++ Exo3/Exo3A.java
@@ -0,0 +1,20 @@
+public class Exo3A {
+    
+        static int[] fibo(int num) {
+            int[] FibNumbers = new int[num];
+            FibNumbers[0] = 0;
+            FibNumbers[1] = 1;
+            for (int i = 2; i < num; i++) {
+                FibNumbers[i] = FibNumbers[i - 1] + FibNumbers[i - 2];
+            }
+            return FibNumbers;
+        }
+
+        public static void main(String[] args) {
+            int num = 10;
+            int[] result = fibo(num);
+            for (int i = 0; i < num; i++) {
+                System.out.print(result[i] + " | ");
+            }
+        }
+}
